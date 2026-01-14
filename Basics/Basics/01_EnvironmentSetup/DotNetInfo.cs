5ef5f4651e6147feaf9243ffb7119d1872133ee9
Shivam Verma <verma.shivam.cs@gmail.com>
2026-01-14 12:58:10 +0530

    Added Basics Folder and .gitignore

---
+++ Basics/Basics/01_EnvironmentSetup/DotNetInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics._01_EnvironmentSetup
+{
+    internal class DotNetInfo
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Basics/Basics/02_BasicElements/Variables.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics._02_BasicElements
+{
+    internal class Variables
+    {
+        public static void Main()
+        {
+
+            // Examples of different Primitive Data Types in C#
+            //Integer
+            int myInt = 10;
+
+            //Double for floating point numbers
+            double myDouble = 20.5;
+
+            // String for Text 
+            string myString = "Hello, World!";
+
+            // Boolean for true/false values 
+            bool myBool = true;
+
+            //Display the variables
+            Console.WriteLine("Integer: " + myInt);
+            Console.WriteLine("Double: " + myDouble);
+            Console.WriteLine("String: " + myString);
+            Console.WriteLine("Boolean: " + myBool);
+
+        }
+    }
+}
