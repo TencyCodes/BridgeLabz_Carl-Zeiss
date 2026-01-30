5bc740114e5777b0c5bc3ddfbe45ee39b40368fe
Shivam Verma <verma.shivam.cs@gmail.com>
2026-01-30 13:06:50 +0530

    Commmited history

--- OOPs/OOP/OOP/obj/Debug/net8.0/OOP.AssemblyInfo.cs
+++ OOPs/OOP/OOP/obj/Debug/net8.0/OOP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db47b7c7a8d75f292da2704f3023a9f1fbd49b33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04360ae01cabf9ff96289cea7fecae01db4c3246")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
