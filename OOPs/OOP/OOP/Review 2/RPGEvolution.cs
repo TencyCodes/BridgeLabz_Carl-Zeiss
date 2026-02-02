f9660f5c56ef978de9dabb237517007de990b5a8
Shivam Verma <verma.shivam.cs@gmail.com>
2026-02-02 16:38:08 +0530

    Review Code Added

---
+++ OOPs/OOP/OOP/Review 2/RPGEvolution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Review_2
+{
+    internal class RPGEvolution
+    {
+        class Warrior
+        {
+            public virtual double attack()
+            {
+                return 100;
+            }
+        }
+        class Paladin : Warrior
+        {
+            public override double attack()
+            {
+                double p = base.attack();
+                return (p * 1.2);
+            }
+            public void heal()
+            {
+                Console.WriteLine("Heal Done");
+            }
+        }
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Warrior a = new Warrior();
+                Console.WriteLine(a.attack());
+                Warrior b = new Paladin();
+                Console.WriteLine(b.attack());
+                Paladin p = new Paladin();
+                p.heal();
+            }
+        }
+
+    }
+}
+
---
+++ OOPs/OOP/OOP/Review 2/UniversalRemote.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Review_2
+{
+    internal class UniversalRemote
+    {
+
+        abstract class Device
+        {
+            public abstract void turn_on();
+        }
+        class Light : Device
+        {
+            public override void turn_on()
+            {
+                Console.WriteLine("Light is Turned On");
+            }
+        }
+        class AC : Device
+        {
+            public override void turn_on()
+            {
+                Console.WriteLine("AC is Turned On");
+            }
+        }
+        class TV : Device
+        {
+            public override void turn_on()
+            {
+                Console.WriteLine("TV is Turned On");
+            }
+        }
+        class RemoteControl
+        {
+            public void press(Device[] d)
+            {
+                for (int i = 0; i < d.Length; i++)
+                {
+                    d[i].turn_on();
+                }
+            }
+        }
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Device[] d = new Device[3];
+                d[0] = new Light();
+                d[1] = new AC();
+                d[2] = new TV();
+
+                RemoteControl r = new RemoteControl();
+                r.press(d);
+            }
+        }
+    }
+}
