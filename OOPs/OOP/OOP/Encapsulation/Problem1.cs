48126a0ed49c47eb0a1eff68d1c02134de7b41a1
Shivam Verma <verma.shivam.cs@gmail.com>
2026-02-11 18:01:02 +0530

    OOPs

---
+++ OOPs/OOP/OOP/Encapsulation/Problem1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP.Encapsulation
+{
+    internal class Problem1
+    {
+        public class TemperatureSensor
+        {
+            private double temp;
+
+            public TemperatureSensor(double temperature)
+            {
+                if (isValidTemp(temperature))
+                {
+                    temp = temperature;
+                }
+                else
+                {
+                    temp = 0;
+                }
+                
+            }
+            public void SetTemp(double temper)
+            {
+                if (isValidTemp(temper))
+                {
+                    temp = temper;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Temperature");
+                }
+            }
+            public bool isValidTemp(double val)
+            {
+                if(val>=-50 && val <= 150)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public double GetTemperature()
+            {
+                return temp;
+            }
+        }
+    }
+}
--- OOPs/OOP/OOP/obj/Debug/net8.0/OOP.AssemblyInfo.cs
+++ OOPs/OOP/OOP/obj/Debug/net8.0/OOP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc740114e5777b0c5bc3ddfbe45ee39b40368fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9660f5c56ef978de9dabb237517007de990b5a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
